9745fbe57f35294e19503e2c793761b133b1bd36
Dimitris Sfounis <sfounisdimitris@gmail.com>
2018-03-10 19:06:45 +0200

    Added website code
    Not needed, but I'll just leave this up here in case anyone wants it.

---
+++ Website code/scripts/main.js
@@ -0,0 +1,7 @@
+require(['scripts/@cityofzion/neon-js'], function (neonjs) {
+    //foo is now loaded.
+});
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello World";
+}
